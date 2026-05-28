3ec05b84af19ddd109a9083ecd28246414456d9e
kyt61767126 <kyt61767126@qq.com>
2026-05-28 15:48:31 +0800

    重建中医处方系统 - 云端版

--- functions/api/test.js
+++
@@ -1,7 +0,0 @@
-export default {
-  async onRequest() {
-    return new Response(JSON.stringify({ message: "Hello from Functions!" }), {
-      headers: { "Content-Type": "application/json" }
-    });
-  }
-};
